c736d3750692ccc10064490a29b7fd230e2201bb
tianyizh <ztykling@gmail.com>
2025-01-18 14:47:51 +0800

    fix quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -17,6 +17,13 @@
 
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(quantity: u32) -> u32 {
+    if quantity > 40 {
+        quantity
+    } else {
+        quantity * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
